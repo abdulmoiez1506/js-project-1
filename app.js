b58319db06d9234e29a8c67d9e21249fc0ba3228
abdul moiez <155585384+abdulmoiez1506@users.noreply.github.com>
2024-01-22 07:44:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+alert('Greeting! welcome to our webpage')
+
+var name = prompt ('whats your name' )
+var age = prompt ('whats your age' )
+
+console.log(name);
+console.log(age);
+
+if(age<25){
+    console.log('you are a student');
+}
+else {
+    console.log('you are not a student');
+}
